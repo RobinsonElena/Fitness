b8c99e14e9b55afb53346cd8d1d0beff08e78fd2
RobinsonElena <sherbakovaen2011@yandex.ru>
2023-05-29 00:06:38 -0400

    Вносит правки по баг-листу

--- source/js/modules/init-swiper.js
+++ source/js/modules/init-swiper.js
@@ -6,29 +6,33 @@ const initSwiperCoach = () => {
       new Swiper('.coaches__slider', { // eslint-disable-line
         direction: 'horizontal',
         loop: true,
+        keyboard: {
+          enabled: true,
+          onlyInViewport: true,
+        },
 
         navigation: {
           nextEl: '.coaches__button--next',
           prevEl: '.coaches__button--prev',
         },
-
+        maxBackfaceHiddenSlides: 0,
         breakpoints: {
           320: {
             slidesPerView: 1,
+            slidesPerGroup: 1,
             spaceBetween: 20,
-            initialSlide: 2,
           },
 
           768: {
             slidesPerView: 2,
+            slidesPerGroup: 2,
             spaceBetween: 30,
-            initialSlide: 2,
           },
 
           1200: {
             slidesPerView: 4,
+            slidesPerGroup: 4,
             spaceBetween: 40,
-            initialSlide: 0,
           },
         },
       })
